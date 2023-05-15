46bb37572e9e626bc9cbef13e047d9c4c8886476
michaelhodgkins <mhodgkins1997@gmail.com>
2023-05-15 13:43:40 +0100

    Player
    -idle/run animations
    -animator setup
    -switches between two depending on input value on the x
    -sprite renderer flips on direction

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -5,24 +5,57 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    bool IsMoving { 
+        set
+        {
+            isMoving = value;
+            animator.SetBool("isMoving", isMoving);
+        }
+    }
     public float moveSpeed = 500f;
     public Vector2 input = Vector2.zero;
     Rigidbody2D rb;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    public bool isMoving = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
 
      void OnMove(InputValue value)
     {
         input = value.Get<Vector2>();
+       
+        
     }
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(input * moveSpeed * Time.fixedDeltaTime);
+        if (input != Vector2.zero)
+        {
+            rb.AddForce(input * moveSpeed * Time.fixedDeltaTime);
+            IsMoving = true;
+
+        }
+        else
+        {
+            IsMoving = false;
+        }
+
+        if (input.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (input.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
     }
 }
